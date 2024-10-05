60be02eceb701eca5b0571d113a150868922a6a7
ghitamikou <mikoughita9@gmail.com>
2024-10-05 21:50:29 +0200

    Rechercher un motif avec la méthode KMP

--- src/RechercheMotif.java
+++ src/RechercheMotif.java
@@ -116,32 +116,5 @@ public class RechercheMotif {
         writer.close();
     }
 
-    public static void main(String[] args) {
-        // le chemin du fichier texte
-        String filePath = "/Users/ghitamikou/Desktop/test.txt";
 
-        // les motifs à chercher
-        List<String> motifs = new ArrayList<>();
-        motifs.add("from");
-        motifs.add("crucial");
-        motifs.add("crucial");
-
-        try {
-            // Lire le contenu du fichier
-            String text = readFileAsString(filePath);
-
-            // Créer un fichier CSV pour stocker les résultats
-            String csvFile = "durations.csv";
-
-            // Chercher plusieurs motifs avec KMP et stocker les résultats dans le CSV
-            chercherPlusieursMotifs(text, motifs, csvFile);
-
-            // Appeler le script Python pour générer le diagramme
-            String pythonCommand = "python3 generate_chart.py " + csvFile;
-            Runtime.getRuntime().exec(pythonCommand);
-
-        } catch (IOException e) {
-            System.err.println("Erreur lors de la lecture du fichier : " + e.getMessage());
-        }
-    }
 }
